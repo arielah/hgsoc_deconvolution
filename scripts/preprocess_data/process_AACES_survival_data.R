605e0e98e5e575cdf1d06efb77dcbc442d6e80d7
Ariel Hippen <ariel.hippen@gmail.com>
2023-01-09 15:36:43 -0500

    Updated preprocessing AACES survival

--- scripts/preprocess_data/process_AACES_survival_data.R
+++ scripts/preprocess_data/process_AACES_survival_data.R
@@ -11,3 +11,18 @@ local_data_path <- params$local_data_path
 
 aaces <- fread(paste(local_data_path, "AACES",
                      "tissuegrant_epidata_08102022.csv", sep = "/"))
+
+id_map <- fread(paste(local_data_path, "AACES",
+                      "main_metadata_table.tsv", sep = "/"))
+
+aaces <- left_join(aaces, id_map)
+aaces <- subset(aaces, select = c("ID", "study", "refage", "diagyear", 
+                                  "vitalstatus", "survival_days", "neoadj", 
+                                  "dblkstat", "seer_stage", "tissue_source",
+                                  "ClusterK4_kmeans_TCGA_names", 
+                                  "external_HGSCsubtype_estimate"))
+
+aaces$ID <- paste("Sample", aaces$ID, sep = "_")
+
+outfile <- paste(local_data_path, "AACES", "AACES_survival.tsv", sep = "/")
+write.table(aaces, file = outfile, sep = "\t", row.names = F, quote = F)
