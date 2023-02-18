38ef44403aa819e62a77232716c00be0e25eddc6
arielah <ariel.hippen@gmail.com>
2023-02-18 16:00:25 -0500

    Run BayesPrism embeddings analysis

---
+++ scripts/evaluation/choose_k_for_embeddings.R
@@ -0,0 +1,44 @@
+suppressPackageStartupMessages({
+  library(data.table)
+  library(SingleCellExperiment)
+  library(dplyr)
+  library(curatedOvarianData)
+  library(yaml)
+  library(stringr)
+  library(ggplot2)
+  library(ggrepel)
+  library(BayesPrism)
+})
+
+params <- read_yaml("../../config.yml")
+data_path <- params$data_path
+local_data_path <- params$local_data_path
+plot_path <- params$plot_path
+
+# Current options are AACES, TCGA (for RNA-seq), microarray, and tothill
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 1) {
+  print(args)
+  dataset <- args[1]
+}
+
+# Load bayesprism object
+bp <- readRDS(paste(local_data_path, "/deconvolution_output/", dataset, 
+                    "_default_bayesprism_results_full.rds", sep = ""))
+
+# Load malignant gene expression
+Z.tum.norm <- t(bp@reference.update@psi_mal)
+
+# Scan a range of K values for number of malignant programs
+estim.Z.tum.norm <- nmf(Z.tum.norm, rank=2:12, seed=123456)
+
+filename <- paste(local_data_path, "/embeddings/", dataset, "_malignant_nmf_rank.rds", sep = "")
+saveRDS(estim.Z.tum.norm, filename)
+
+png(paste(plot_path, "/embedding_plots/", dataset, "_nmf_rank.png", sep = ""), width = 800)
+plot(estim.Z.tum.norm)
+dev.off()
+
+png(paste(plot_path, "/embedding_plots/", dataset, "_consensus_map.png", sep = ""), width = 800)
+consensusmap(estim.Z.tum.norm, labCol=NA, labRow=NA)
+dev.off()
